d69578feb7f9d1cf4a89b7535abbc1754b31d3c1
Zentrik <Zentrik@users.noreply.github.com>
2024-03-11 01:47:27 +0000

    Some horrific code to print benchmark code

--- site/frontend/src/pages/compare/compile/table/shortcuts/cachegrind-cmd.vue
+++ site/frontend/src/pages/compare/compile/table/shortcuts/cachegrind-cmd.vue
@@ -33,23 +33,79 @@ function normalizeScenario(scenario: string): string {
   }
   return "<invalid scenario>";
 }
+
+function getsuite(benchmark: string): string {
+  return benchmark.split('.', 1)[0];
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function normalizeBenchmark(benchmark: string): string {
+  let i = benchmark.search("\\.\\(");
+  if (i == -1) {
+    let parts = benchmark.split('.');
+    let result = [parts[0], parts[1]];
+    if (parts.length > 2) {
+      result.push(parts.slice(2).join('.'));
+    }
+    return result.map((x) => "\"" + x + "\"").join(", ");
+  } else {
+      let parts = benchmark.slice(0, i).split('.');
+      let result = [parts[0]];
+      if (parts.length > 1) {
+        result.push(parts.slice(1).join('.'));
+      }
+      parts = result.map((x) => "\"" + x + "\"");
+
+      let braces = 0;
+      let start_idx = i + 2;
+      let end_idx = i + 2;
+      let split_tuple = [];
+
+      while (end_idx < benchmark.length-1) {
+        if (benchmark[end_idx] == "\{") {
+          braces += 1;
+        } else if (benchmark[end_idx] == "\}") {
+          braces -= 1;
+        } else if (benchmark[end_idx] == "," && braces == 0) {
+          split_tuple.push(benchmark.slice(start_idx, end_idx));
+
+          start_idx = end_idx + 1;
+        }
+
+        end_idx += 1;
+      }
+      split_tuple.push(benchmark.slice(start_idx, end_idx));
+
+      // let split_tuple = benchmark.slice(i+2, benchmark.length-1).split(',');
+      let quoted_tuple = split_tuple.map((x) => isNumeric(x) ? x.trimStart() : "\"" + x.trimStart() + "\"");
+      let stringified_tuple = '\(' + quoted_tuple.join(", ").trimStart() + '\)';
+
+      return parts.concat(stringified_tuple).join(", ");
+  }
+}
 </script>
 
 <template>
-  <pre><code>./target/release/collector profile_local cachegrind \
-    +{{ firstCommit }} \<template v-if="props.baselineCommit !== undefined">
-    --rustc2 +{{ props.commit }} \</template>
-    --include {{ testCase.benchmark }} \
-    --profiles {{ normalizeProfile(testCase.profile) }} \
-    --scenarios {{ normalizeScenario(testCase.scenario) }}</code></pre>
+  <pre><code>using BaseBenchmarks
+BaseBenchmarks.load!("{{ getsuite(testCase.benchmark) }}")
+run(BaseBenchmarks.SUITE[[{{ normalizeBenchmark(testCase.benchmark) }}]])</code></pre>
 </template>
 
 <style scoped lang="scss">
 pre {
   background-color: #eeeeee;
+  padding: 5px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  border-radius: 10px;         /* Rounded corners */
+  white-space: pre-wrap;       /* Since CSS 2.1 */
+  word-wrap: break-word;       /* Internet Explorer 5.5+ */
 }
 
 code {
-  user-select: all;
+  /* user-select: all; */
 }
 </style>
