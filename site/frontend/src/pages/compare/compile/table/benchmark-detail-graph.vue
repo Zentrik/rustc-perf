7eb8d389c3116ca4481afec2d01a379ee44417b3
Zentrik <Zentrik@users.noreply.github.com>
2024-03-19 19:56:11 +0000

    Hide graph name under expanded comparisons
    It's redundant and for long names wastes a lot of space. Reverts earlier commit that was likely me trying to get rid of something eles and forgetting to restore this setting.

--- site/frontend/src/pages/compare/compile/table/benchmark-detail-graph.vue
+++ site/frontend/src/pages/compare/compile/table/benchmark-detail-graph.vue
@@ -110,6 +110,7 @@ async function renderGraph(
 ) {
   const opts: GraphRenderOpts = {
     width: Math.min(window.innerWidth / 2 - 40, 380),
+    renderTitle: false
   };
   if (date !== null) {
     drawCurrentDate(opts, date);
