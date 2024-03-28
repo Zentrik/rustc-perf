83b1aa759a7c589c1f5a1f04699e0e3909aaae8f
Zentrik <Zentrik@users.noreply.github.com>
2024-03-28 18:34:04 +0000

    Try to stop server crashing?

--- Orchestrator.jl
+++ Orchestrator.jl
@@ -31,22 +31,28 @@ function main()
         now = Dates.now()
         sleep(1) # little buffer to make sure now <= mtime(dir) is true
 
-        LibGit2.fetch(repo)
-        LibGit2.merge!(repo, fastforward=true)
+        try
+            LibGit2.fetch(repo)
+            LibGit2.merge!(repo, fastforward=true)
 
-        changed = false
-        for rel_dir in ("by_date", "by_hash")
-            for benchmark_dir in readdir(joinpath(nanosoldier_dir, "benchmark", rel_dir), join=true)
-                if isdir(benchmark_dir) && now <= unix2datetime(mtime(joinpath(benchmark_dir, benchmark_dir)))
-                    changed = true
-                    kill_server()
-                    println("$(joinpath(rel_dir, benchmark_dir)) changed")
-                    process_benchmarks(benchmark_dir)
+            changed = false
+            for rel_dir in ("by_date", "by_hash")
+                for benchmark_dir in readdir(joinpath(nanosoldier_dir, "benchmark", rel_dir), join=true)
+                    if isdir(benchmark_dir) && now <= unix2datetime(mtime(joinpath(benchmark_dir, benchmark_dir)))
+                        changed = true
+                        kill_server()
+                        println("$(joinpath(rel_dir, benchmark_dir)) changed")
+                        process_benchmarks(benchmark_dir)
+                    end
                 end
             end
-        end
 
-        if changed
+            if changed
+                start_server()
+            end
+        catch err
+            println("Error: $err") # Sometimes fetch fails
+            kill_server()
             start_server()
         end
         sleep(sleep_time)
