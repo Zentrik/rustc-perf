984dcdbc7b89d3e82c068af5dfbe3196babadd91
Zentrik <Zentrik@users.noreply.github.com>
2024-04-04 16:33:08 +0100

    Orchestrator: clean up

--- Orchestrator.jl
+++ Orchestrator.jl
@@ -31,17 +31,23 @@ function main()
         now = now(UTC)
         sleep(1) # little buffer to make sure now <= mtime(dir) is true
 
+        fetched = false
         try
             LibGit2.fetch(repo)
             LibGit2.merge!(repo, fastforward=true)
+            fetched = true
+        catch err
+            println("Error: $err") # Sometimes fetch fails
+        end
 
+        if fetched
             changed = false
             for rel_dir in ("by_date", "by_hash")
                 for benchmark_dir in readdir(joinpath(nanosoldier_dir, "benchmark", rel_dir), join=true)
-                    if isdir(benchmark_dir) && now <= unix2datetime(mtime(joinpath(benchmark_dir, benchmark_dir)))
+                    if isdir(benchmark_dir) && now <= unix2datetime(mtime(benchmark_dir))
                         changed = true
                         kill_server()
-                        println("$(joinpath(rel_dir, benchmark_dir)) changed")
+                        println("$(benchmark_dir) changed")
                         process_benchmarks(benchmark_dir)
                     end
                 end
@@ -50,11 +56,8 @@ function main()
             if changed
                 start_server()
             end
-        catch err
-            println("Error: $err") # Sometimes fetch fails
-            kill_server()
-            start_server()
         end
+
         sleep(sleep_time)
     end
 end
